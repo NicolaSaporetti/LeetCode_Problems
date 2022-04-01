af2f5b12779ea30e543eb21d29404b5cb4f596ab
NicolaSaporetti <nicola.saporetti@gmail.com>
2022-04-01 11:05:54 +0200

    Combinations solved

---
+++ CombinationSums2.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+    private:
+    void order(vector<int>& vec)
+    {
+        for(int i=0; i< vec.size(); i++)
+        {
+            for(int j=i+1; j< vec.size(); j++)
+            {
+                if(vec[i]>vec[j])
+                {
+                    int temp = vec[j];
+                    vec[j] = vec[i];
+                    vec[i] = temp;
+                }
+            }
+        }
+    }
+
+    bool is_duplicate()
+    {
+        bool found = false;
+        for(int i=0; i< solutions.size() && !found; i++)
+        {
+            if(temp.size()==solutions[i].size())
+            {
+                bool mismatchFound = false;
+                for(int j=0;j<temp.size() && !mismatchFound;j++)
+                {
+                    if(solutions[i][j]!=temp[j])
+                    {
+                        mismatchFound = true;
+                    }
+                }
+                if(!mismatchFound)
+                {
+                    found = true;
+                }
+            }
+        }
+        return found;
+    }
+
+    void addNumber(vector<int>& candidates, int index_start) {
+        if(temp_sum>=target)
+        {
+            if(temp_sum==target && !is_duplicate())
+            {
+                solutions.push_back(temp);
+            }
+            return;
+        }
+        else
+        {
+            for(int i=index_start;i<candidates.size();i++)
+            {
+                temp.push_back(candidates[i]);
+                temp_sum+=candidates[i];
+                addNumber(candidates, i+1);
+                temp.pop_back();
+                temp_sum-=candidates[i];
+                for(; i+1<candidates.size() && candidates[i]==candidates[i+1];i++);
+            }
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        solution_index=0;
+        temp_sum=0;
+        this->target=target;
+        order(candidates);
+        addNumber(candidates,0);
+        return solutions;
+    }
+
+    int solution_index;
+    int target;
+    int temp_sum;
+    vector<vector<int>> solutions;
+    vector<int> temp;
+};
+
+int main()
+{
+    vector<int> numbers = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    Solution solution;
+    vector<vector<int>> solutions = solution.combinationSum2(numbers,30);
+    for(int i=0; i< solutions.size(); i++)
+    {
+        for(int j=0;j<solutions[i].size();j++)
+        {
+            cout<<solutions[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

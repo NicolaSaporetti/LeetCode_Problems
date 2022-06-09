f6fcdf7f875c30d87cfacef3d559159b8aadcccd
Nicola Saporetti <nicola.saporetti@asml.com>
2022-06-09 07:44:35 +0200

    Changed List folder name to Linked Lists

---
+++ Linked Lists/DetectCycle.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        if(fast==nullptr || fast->next == nullptr) return nullptr;
+        do
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        } while (fast!=nullptr && fast->next!=nullptr && fast!=slow);
+        if(fast==nullptr || fast->next == nullptr) return nullptr;
+        else
+        {
+            fast = head;
+            while(fast!=slow)
+            {
+                fast= fast->next;
+                slow=slow->next;
+            }
+        }
+        return fast;
+    }
+};

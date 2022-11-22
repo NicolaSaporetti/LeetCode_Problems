79f0de759fd6e92f7adf53608bfbfefc676123d3
Nicola Saporetti <nicola.saporetti@gmail.com>
2022-11-22 17:55:36 +0100

    Changes till 22/11/2022 (#27)
    Added problems from 205 to 210

--- LeetCodeProblems/Trees/DD/Trie.cpp
+++ LeetCodeProblems/Trees/DD/Trie.cpp
@@ -1,19 +1,73 @@
-#include <array>
+#include <vector>
+
+template<class T>
+class Node {
+public:
+    T val;
+    bool wordFinished;
+    vector<Node<T>*> children;
+
+    Node() {}
+
+    Node(T val) : val(val) {}
+};
 
 class Trie {
-    public:
-        array<Trie*,26> ch;
-        int visited = 0;
-        bool endWord;
-
-        void insert(string& s)
+public:
+    Trie() {
+        root = new Node<char>('*');
+    }
+    
+    void insert(string word) {
+        word.push_back('.');
+        Node<char>* temp = root;
+        for(int i=0;i<word.size();i++)
         {
-            auto t = this;
-            for (char& c: s) {
-                if (!t->ch[c - 'a']) t->ch[c - 'a'] = new Trie();
-                t->ch[c - 'a']->visited++;
-                t = t->ch[c - 'a'];
+            bool nodeFound = false;
+            for(int j=0;j<temp->children.size();j++)
+            {
+                if(temp->children[j]->val==word[i])
+                {
+                    temp = temp->children[j];
+                    nodeFound = true;
+                    break;
+                }
             }
-            t->endWord = true;
+            if(!nodeFound)
+            {
+                temp->children.push_back(new Node<char>(word[i]));
+                temp = temp->children[temp->children.size()-1];
+            }
         }
+    }
+    
+    bool search(string word) {
+        word.push_back('.');
+        return startsWith(word);
+    }
+    
+    bool startsWith(string prefix) {
+        Node<char>* temp = root;
+        for(int i=0;i<prefix.size();i++)
+        {
+            bool nodeFound = false;
+            for(int j=0;j<temp->children.size();j++)
+            {
+                if(temp->children[j]->val==prefix[i])
+                {
+                    temp = temp->children[j];
+                    nodeFound = true;
+                    break;
+                }
+            }
+            if(!nodeFound)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+private:    
+    Node<char>* root;
 };
